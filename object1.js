3d8e7f29a136df64d72448cfb0cb5093f60fc995
Sarreha Tasmin <81848071+sarrehatasmin@users.noreply.github.com>
2021-04-14 22:42:02 +0600

    Update object1.js

--- object1.js
+++ object1.js
@@ -11,3 +11,16 @@ console.log(student1.name);
 console.log(student1.age);
 console.log(student1.lang);
 console.log(student1.district);
+function student(name,district,age,cgpa,lang){
+    this.name=name;
+    this.district=district;
+    this.age=age;
+    this.cgpa=cgpa;
+    this.lang=lang;
+}
+var student1=new student("rikta","naogaon",25,3.97,"Bangla, English,Somali");
+console.log(student1.name);
+console.log(student1.district);
+console.log(student1.age);
+console.log(student1.cgpa);
+console.log(student1.lang);
