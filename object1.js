eba00a1eff0f573c84c88ac751b82fe5ededdf33
Sarreha Tasmin <81848071+sarrehatasmin@users.noreply.github.com>
2021-04-14 22:27:48 +0600

    object.html

---
+++ object1.js
@@ -0,0 +1,13 @@
+var student1={
+    name:"Sarreha",
+    district:"naogaon",
+    age:23,
+    cgpa:3.95,
+    lang:"Bangla, English,Hindi",
+
+
+}
+console.log(student1.name);
+console.log(student1.age);
+console.log(student1.lang);
+console.log(student1.district);
