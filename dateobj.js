d356cdccacf16879e3434bb80971e5fcdfea6b82
Sarreha Tasmin <81848071+sarrehatasmin@users.noreply.github.com>
2021-04-16 23:37:44 +0600

    Create dateobj.js

---
+++ dateobj.js
@@ -0,0 +1,16 @@
+var date=new Date();
+console.log(date);
+var month=date.getMonth();
+console.log(month);
+var year=date.getFullYear();
+console.log(year);
+var currentdate=date.getDate();
+console.log(currentdate);
+var currentmonth=date.getMonth();
+console.log(currentmonth);
+var currenthour=date.getHours();
+console.log(currenthour);
+var currentminute=date.getMinutes();
+console.log(currentminute);
+var currentsec=date.getMilliseconds();
+console.log(currentsec);
